5d2f92a7c9dfded9bda928eba51dd91ed0582016
tanshuo <shuotan@ShuoTandeMac-mini.local>
2016-10-31 23:32:38 -0700

    add GPUImage filter

---
+++ LuluBroadcaster/Logic/Helper/BeautifyFilter.m
@@ -0,0 +1,14 @@
+//
+//  BeautifyFilter.m
+//  LuluBroadcaster
+//
+//  Created by ShuoTan on 10/31/16.
+//  Copyright © 2016 ShuoTan. All rights reserved.
+//
+
+#import "BeautifyFilter.h"
+#import <GPUImage/GPUImage.h>
+
+@implementation BeautifyFilter
+
+@end
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ platform :ios, '9.0'
 target 'LuluBroadcaster' do
 pod 'AFNetworking', '~> 3.0'
 pod 'SAMKeychain', :git => 'https://github.com/soffes/SAMKeychain'
+pod 'GPUImage'
 end
