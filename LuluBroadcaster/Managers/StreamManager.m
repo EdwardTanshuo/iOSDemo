f2aebfb4f539ed45baa5953d960824110517a412
tanshuo <shuotan@ShuoTandeMac-mini.local>
2016-11-25 19:45:37 -0800

    optimize sleep mode

--- LuluBroadcaster/Managers/StreamManager.m
+++ LuluBroadcaster/Managers/StreamManager.m
@@ -77,8 +77,8 @@
         return;
     self.session = [[VCRtmpSession alloc] initWithVideoSize:VIDEO_SIZE_CIF fps:12 bitrate:BITRATE_CIF];
     SettingSession* setting = [[SettingSession alloc] init];
-    [self.session startRtmpSession:@"rtmp://10.10.17.182:1935/rtmplive/kjkjkj"];
-    //[self.session startRtmpSession:[NSString stringWithFormat:@"%@/%@", setting.url, setting.streamKey]];
+    //[self.session startRtmpSession:@"rtmp://10.10.17.182:1935/rtmplive/kjkjkj"];
+    [self.session startRtmpSession:[NSString stringWithFormat:@"%@/%@", setting.url, setting.streamKey]];
     //_timer = [NSTimer scheduledTimerWithTimeInterval:0.04 target:self selector:@selector(rtmpProcess) userInfo:nil repeats:YES];
     _isStreaming = YES;
 }
