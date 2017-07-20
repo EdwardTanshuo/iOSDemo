92ca7691d927f009d02838a8e512d3cf7efb0243
tanshuo <tanshuo198956@gmail.com>
2017-07-20 11:21:34 +0800

    mosaic

---
+++ LuluBroadcaster/Managers/LiveManager.h
@@ -0,0 +1,33 @@
+//
+//  LiveManager.h
+//  LuluBroadcaster
+//
+//  Created by ShuoTan on 10/31/16.
+//  Copyright © 2016 ShuoTan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@protocol LiveDataSourceDelegate
+- (void) recieveStichedFragment:(CVPixelBufferRef)pixelBuffer timestamp:(int64_t)timestamp;
+- (void) recieveOnRawFragment:(CVPixelBufferRef)pixelBuffer timestamp:(int64_t)timestamp;
+- (void) recieveError:(NSError *)error;
+- (void) recieveFaceCoor: (CGRect)rect;
+@end
+
+
+@interface LiveManager : NSObject
+@property (nonatomic, assign) BOOL isLiving;
+@property (nonatomic, weak) id<LiveDataSourceDelegate> delegate;
+
+
+#pragma mark singleton
++ (LiveManager*)sharedManager;
+
+#pragma mark public methods
+- (void)startLiveWithView: (UIView*) view;
+- (void)stopLive;
+
+@end
+
